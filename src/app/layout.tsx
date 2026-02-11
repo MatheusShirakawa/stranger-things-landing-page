0b8ae9bf1341883cedf3703b58ca96191b8c3b95
MatheusShirakawa <matheus_shirakawa@outlook.com>
2026-02-10 22:18:08 -0300

    feat: :rocket: adding a favicon and changing the title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import { Archivo, Poppins } from "next/font/google";
 const archivo = Archivo({ subsets: ["latin"] });
 const poppins = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
+export const metadata = {
+  title: 'Stranger Things Landing Page',
+  description: 'A landing page inspired by the Stranger Things series, built with Next.js and GSAP for smooth animations.',
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
